102c0c30ba5b21d4226b4a06b560e9342a996173
macyapp <sidhantn99@gmail.com>
2020-05-14 13:14:53 +0530

    Initial commit

--- chapter-2/#02_5.py
+++ chapter-2/#02_5.py
@@ -3,3 +3,6 @@ Exercise 5: Write a program which prompts the user for a Celsius tem-
 perature, convert the temperature to Fahrenheit, and print out the
 converted temperature.
 """
+c=float(input("Enter temperature in celsius: "))
+f=1.8*c+32
+print("Temperature in celsius: ",f)
