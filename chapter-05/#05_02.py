e4cf8027fd1dda468d05278060d130a44bf003eb
macyapp <sidhantn99@gmail.com>
2020-05-22 11:03:44 +0530

    Initial commit

---
+++ chapter-05/#05_02.py
@@ -0,0 +1,5 @@
+"""
+Exercise 2: Write another program that prompts for a list of numbers
+as above and at the end prints out both the maximum and minimum of
+the numbers instead of the average.
+"""
---
+++ chapter-06/#06_03.py
@@ -0,0 +1,4 @@
+"""
+Exercise 3: Encapsulate this code in a function named count, and gen-
+eralize it so that it accepts the string and the letter as arguments.
+"""
