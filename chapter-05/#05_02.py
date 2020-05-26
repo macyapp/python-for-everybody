a4dfb41769b930e712e674062e694327288293a0
macyapp <sidhantn99@gmail.com>
2020-05-26 09:29:00 +0530

    Initial commit

--- chapter-05/#05_02.py
+++ chapter-05/#05_02.py
@@ -3,24 +3,19 @@ Exercise 2: Write another program that prompts for a list of numbers
 as above and at the end prints out both the maximum and minimum of
 the numbers instead of the average.
 """
-total=0
-n=0
 max=None
 min=None
 while True:
-   num=None
+   num=input("Enter a number: ")
+   if num=="Done" or num=="done":
+      break
    try:
-      num=input("Enter a number: ")
-      if num=="Done" or num=="done":
-         break
       num=int(num)
-      if min is None or num<min:
-         min=num
-      if num is None or num>max:
-         max=num
-      total+=num
-      n+=1
    except:
       print("Invalid input")
-avg=total/n
-print(total,n,max,min)
+      continue
+   if max is None or num>max :
+      max =num
+   if min is None or num < min :
+      min = num
+print("Max:",max,"; Min:",min)
