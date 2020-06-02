86e1b102c3621d57fae06973ce80e51fd93628d2
macyapp <sidhantn99@gmail.com>
2020-06-02 12:37:00 +0530

    Initial commit

---
+++ chapter-11/eg1.py
@@ -0,0 +1,6 @@
+import re
+hand = open('/home/sidhant/Prototype/python-for-everybody/chapter-11/mbox.txt')
+for line in hand:
+   line = line.rstrip()
+   if re.search('From:', line):
+      print(line)
