b184386bae6e6fa9a2c9e343acfe7e10dfa56203
macyapp <sidhantn99@gmail.com>
2020-06-26 09:57:37 +0530

    Update

--- chapter-12/eg2.py
+++ chapter-12/eg2.py
@@ -5,13 +5,13 @@ import time
 HOST = 'data.pr4e.org'
 PORT = 80
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect((HOST, PORT))
+mysock.connect((HOST,PORT))
 mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
 count = 0
 picture = b""
 
 while True:
-    data = mysock.recv(5120)
+    data=mysock.recv(5120)
     if len(data) < 1:
         break
     #time.sleep(0.25)
@@ -22,12 +22,12 @@ while True:
 mysock.close()
 
 # Look for the end of the header (2 CRLF)
-pos = picture.find(b"\r\n\r\n")
-print('Header length', pos)
+pos=picture.find(b"\r\n\r\n")
+print('Header length',pos)
 print(picture[:pos].decode())
 
 # Skip past the header and save the picture data
-picture = picture[pos+4:]
-fhand = open("stuff.jpg", "wb")
+picture=picture[pos+4:]
+fhand=open("stuff.jpg","wb")
 fhand.write(picture)
 fhand.close()
