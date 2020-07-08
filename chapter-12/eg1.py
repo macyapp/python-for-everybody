7270cebedbeea44a6d90d08cad23a5c75a0a0bdf
macyapp <sidhantn99@gmail.com>
2020-07-08 08:51:23 +0530

    Update

--- chapter-12/eg1.py
+++ chapter-12/eg1.py
@@ -9,7 +9,7 @@ cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
-    data=mysock.recv(512)
+    data=mysock.recv(1024)
     if len(data)<1:
         break
     print(data.decode(),end=' ')
